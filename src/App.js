f6ac1e2090e1055022514570f181a9d0f7f172e8
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-09 11:16:51 +0300

    minor updates and basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import FullPost from "./components/FullPost";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import UserPage from "./components/UserPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/postDetails" element={<FullPost />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/Register" element={<Register />} />
+        <Route exact path="/userPage" element={<UserPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FullPost/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullPost = () => {
+    return (
+      <div>
+        <h1>FullPost</h1>
+      </div>
+    );
+}
+
+export default FullPost
